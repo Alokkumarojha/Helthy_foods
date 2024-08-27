389c763fdf1f8ca57d0af9ba42d0ddbf2bd732f8
aojha@Shyam <aojha@gmail.com>
2024-08-27 13:58:01 +0530

    condition randering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 
 function App() {
-  let foodItems = ["Apple", "Banana", "Dall", "Green veg.", "Orange", "Ghee"];
+  let foodItems = ["Dall", "Channa", "Roati", "Pulaw", "Banana"];
+  let emptyMessage = foodItems.length === 0 ? <h1>I am still hungry</h1> : null;
   return (
     <>
       <h1>Helthy Foods</h1>
+      <h2>For helthy person</h2>
+      {emptyMessage}
       <ul class="list-group">
         {foodItems.map((items) => (
           <li key={items} class="list-group-item">
